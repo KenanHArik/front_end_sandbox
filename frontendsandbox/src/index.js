e4e93148c44f3e0dcafe48353ca0e17dbf54cea3
KenanHArik <arik.kenan@gmail.com>
2019-01-31 22:30:54 -0800

    added minidrawer element and styling

--- frontendsandbox/src/index.js
+++ frontendsandbox/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import MiniDrawer from './MiniDrawer/MiniDrawer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({ typography: { useNextVariants: true } });
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <MiniDrawer />
+  </ThemeProvider>,
+  document.querySelector("#root")
+);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
